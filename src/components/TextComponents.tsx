34e9bc6d1e209adcb8c3a6a0d9dc41cc19fd3738
khojwar <khojwartikaram@gmail.com>
2025-09-13 09:53:53 +0545

    props - props drilling

--- src/components/TextComponents.tsx
+++ src/components/TextComponents.tsx
@@ -3,11 +3,19 @@ interface IDescriptionText {
     className?: string;
 }
 
+const TextToPrint = ({text}: Readonly<IDescriptionText>) => {
+    return (
+        <div>
+            <p>{text}</p>
+        </div>
+    )
+}
+
 const TextComponents = ({text, className}: Readonly<IDescriptionText>) => {
   return (
     <div>
         <div className={`p-4 text-justify ${className}`}>
-            <p>{text}</p>
+            <TextToPrint text={text} />
         </div>
     </div>
   )
