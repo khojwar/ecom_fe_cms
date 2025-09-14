9046089b35757a7e5325cef825e3f6430eb0e925
khojwar <khojwartikaram@gmail.com>
2025-09-14 16:51:54 +0545

    router - query parameter

--- src/pages/auth/ActivateUser.tsx
+++ src/pages/auth/ActivateUser.tsx
@@ -1,13 +1,18 @@
-import { useParams } from "react-router";
+import { useParams, useSearchParams } from "react-router";
 
 const ActivateUser = () => {
 
     let params = useParams();
 
+    let [searchParams] = useSearchParams();
+    // let [searchParams, setSearchParams] = useSearchParams();
+
+
 
     return (
         <div>
             <h2>Activate User Page</h2>
+            <p>Query Param - ref: {searchParams.get('name')}</p>
             <p>{params.token}</p>
         </div>
     )
