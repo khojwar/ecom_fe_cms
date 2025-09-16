4aaaa7fbfdb345788f9972039862eaba2a716745
khojwar <khojwartikaram@gmail.com>
2025-09-16 23:09:32 +0545

    yup and hookform resolver used in forget-password form

--- src/components/auth/ForgotPasswordForm.tsx
+++ src/components/auth/ForgotPasswordForm.tsx
@@ -1,30 +1,49 @@
-import { Button, Input } from "antd";
+import { Button } from "antd";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import { useForm } from "react-hook-form";
+import FormInput from "../form/FormInput";
 
-const ForgotPasswordForm = () => {
+const schema = yup.object().shape({
+    email: yup.string().email().required(),
+  })
+
+const ForgotPasswordForm = ({email} : Readonly<{email: string}>) => {
+
+  const { control, handleSubmit, formState: { errors } } = useForm<{email: string}>({
+    defaultValues: {
+      email: email || '',
+    },
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data: any) => {
+    console.log("Form Submitted: ", data);
+  };
+
   return (
     <div>
       <h2 className="text-2xl text-teal-900 shadow-2xl font-bold text-center mb-6">Forgot Password</h2>
 
-      <form className="space-y-4">
+      <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
         <div className="flex items-center">
           <label className="block text-sm font-medium mb-1 w-1/4">Email</label>
-          {/* antd Input component for email */}
-          <Input
-            type="email"
-            className="w-3/4 px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-teal-900!"
-            placeholder="Enter your registered email"
-            required
-          />
+         <div className="w-3/4 flex flex-col">
+            <FormInput control={control} name="email" />
+            {errors.email && (<div className="text-red-500 text-sm ml-4 italic">{errors.email?.message}</div>)}
+         </div>
         </div>
 
-        {/* antd Button component for submit */}
-        <Button
-          type="primary"
-          htmlType="submit"
-          className="w-full bg-teal-900! text-white! py-2! rounded-lg! hover:bg-teal-950!"
-        >
-          Send Reset Link
-        </Button>
+        <div className="text-center">
+          {/* antd Button component for submit */}
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="w-full bg-teal-900! text-white! py-2! rounded-lg! hover:bg-teal-950!"
+          >
+            Send Reset Link
+          </Button>
+        </div>
       </form>
 
       <div className="flex items-center my-6">
